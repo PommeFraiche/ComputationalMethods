f2cc4b1a2d4d71f95fbe320e301456f864b5c84d
dami973 <44095143+dami973@users.noreply.github.com>
2018-10-31 11:01:34 +0000

    Add files via upload

--- ImplicitUpwind.cpp
+++ ImplicitUpwind.cpp
@@ -5,15 +5,16 @@ ImplicitUpwind::ImplicitUpwind(double dX, double dT, double u, double mX, double
 	Scheme(dX, dT, u, mX, mT);
 }
 
-Matrix ImplicitUpwind::solveUpwind(Matrix solve) {
+Matrix ImplicitUpwind::solveUpwind2(Matrix solve) {
 	int width = maxX / deltaX ;
 	int length = maxT / deltaT ;
 	Matrix A(length, width);
 	vector<double> B(width), X(width);
 	
 	double a = u * deltaT / deltaX, m;
-
-
+	
+	// The problem can be represent the equation of matrix A*X=B
+	//declaration of the matrix A values
 	for (int i = 0 ; i < length; i++) {
 		for (int j = 0; j < width; j++) {
 			if (i == j) A[i][j] = -1 - a;
@@ -22,13 +23,26 @@ Matrix ImplicitUpwind::solveUpwind(Matrix solve) {
 		}
 	}
 
-	for (int k = 1; k < width; k++) {
-		m = A[k][k - 1] / A[k - 1][k - 1];
-		A[k][k] -= (m*A[k - 1][k]);
-		B[k] -= (m*B[k-1]);
-	}
+	//solve for each time (line of solve matrix)
+	for (int i = 0; i <= length; i++) {
 
-	//solve[i][j] = solve[i - 1][j] /(1 - a) + solve[i][j - 1] * a/(1+a);
+		//declaration of vector B value
+		for (int k = 0; k < width; k++)B[k] = solve[i][k];
+
+		//Thomas Algorithm 
+		for (int k = 1; k < width; k++) {
+			m = A[k][k - 1] / A[k - 1][k - 1];
+			A[k][k] -= (m*A[k - 1][k]);
+			B[k] -= (m*B[k - 1]);
+		}
+		X[width - 1] = B[width - 1] / A[width - 1][width - 1];
+		for (int k = width - 2; k >= 0; k--) {
+			X[k] = (B[k] - A[k][k + 1] * X[k + 1]) / A[k][k];
+		}
+
+		//set the result in solve and restart for the next time 
+		for (int k = 0; k < width; k++)solve[i + 1][k] = X[k];
+	}
 
 	return solve;
 }
