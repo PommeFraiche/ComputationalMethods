1c9e0be2fb06d8108e26d77717f4ce9c87f47e53
PommeFraiche <36454174+PommeFraiche@users.noreply.github.com>
2018-11-02 17:10:35 +0000

    Update LaxWendroff.cpp

--- LaxWendroff.cpp
+++ LaxWendroff.cpp
@@ -7,7 +7,7 @@ LaxWendroff::LaxWendroff(double dx, double dt, double u, double xmax, double tma
 Matrix LaxWendroff::solve(Matrix solution) {
 	for (int i = 1; i < n; i++) {
 		for (int j = 1; j < m-1; j++) {
-			solution[i][j] = solution[i - 1][j] * (1 - C * C) + (solution[i - 1][j + 1] + solution[i - 1][j - 1]) * (C*C - C) / 2  ;
+			solution[i][j] = solution[i - 1][j] * (1 - C * C) + solution[i - 1][j + 1]*(C*C-C)/2 + solution[i - 1][j - 1] * (C*C + C) / 2  ;
 		}
 	}
 
