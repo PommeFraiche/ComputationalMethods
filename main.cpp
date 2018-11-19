6ce91745e05c78e6e9944e43abb8b01f2128a44a
PommeFraiche <36454174+PommeFraiche@users.noreply.github.com>
2018-11-19 16:49:45 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 //Problem
 double dx = 5, dt = 0.02, xmax = 405, tmax = 0.5+dt, u = 250;
-Matrix boundary, solution, analitical;
+Matrix boundary, solution, analytical;
 
 int main() {
 	
@@ -19,57 +19,57 @@ int main() {
 	ExplicitUpwind ExpUp(dx, dt, u, xmax, tmax);
 	boundary = ExpUp.boundary();
 	solution = ExpUp.solve(boundary);
-	analitical = ExpUp.analitical();
+	analytical = ExpUp.analytical();
 
 	ExpUp.printSolution(solution);
 
 	//Errors
-	cout << "Err1: "<< ExpUp.ErrorNorm1(analitical, solution)<<endl;
-	cout << "Err2: " << ExpUp.ErrorNorm2(analitical, solution) << endl;
-	cout << "ErrInf: " << ExpUp.ErrorNormInf(analitical, solution) << endl;
+	cout << "Err1: "<< ExpUp.ErrorNorm1(analytical, solution)<<endl;
+	cout << "Err2: " << ExpUp.ErrorNorm2(analytical, solution) << endl;
+	cout << "ErrInf: " << ExpUp.ErrorNormInf(analytical, solution) << endl;
 	
 
 	
 	ImplicitUpwind ImpUp(dx, dt, u, xmax, tmax);
 	boundary = ImpUp.boundary();
 	solution = ImpUp.solve(boundary);
-	analitical = ImpUp.analitical();
+	analytical = ImpUp.analytical();
 
 	ImpUp.printSolution(solution);
 
 	//Errors
-	cout << "Err1: " << ImpUp.ErrorNorm1(analitical, solution) << endl;
-	cout << "Err2: " << ImpUp.ErrorNorm2(analitical, solution) << endl;
-	cout << "ErrInf: " << ImpUp.ErrorNormInf(analitical, solution) << endl;
+	cout << "Err1: " << ImpUp.ErrorNorm1(analytical, solution) << endl;
+	cout << "Err2: " << ImpUp.ErrorNorm2(analytical, solution) << endl;
+	cout << "ErrInf: " << ImpUp.ErrorNormInf(analytical, solution) << endl;
 	
 
 	
 	FTCS ftcs(dx, dt, u, xmax, tmax);
 	boundary = ftcs.boundary();
 	solution = ftcs.solve(boundary);
-	analitical = ftcs.analitical();
+	analytical = ftcs.analytical();
 
 	ftcs.printSolution(solution);
 
 	//Errors
-	cout << "Err1: " << ftcs.ErrorNorm1(analitical, solution) << endl;
-	cout << "Err2: " << ftcs.ErrorNorm2(analitical, solution) << endl;
-	cout << "ErrInf: " << ftcs.ErrorNormInf(analitical, solution) << endl;
+	cout << "Err1: " << ftcs.ErrorNorm1(analytical, solution) << endl;
+	cout << "Err2: " << ftcs.ErrorNorm2(analytical, solution) << endl;
+	cout << "ErrInf: " << ftcs.ErrorNormInf(analytical, solution) << endl;
 	
 
 	LaxWendroff Lax(dx, dt, u, xmax, tmax);
 	boundary = Lax.boundary();
 	solution = Lax.solve(boundary);
-	analitical = Lax.analitical();
+	analytical = Lax.analytical();
 
 	Lax.printSolution(solution);
 
 	//Errors
-	cout << "Err1: " << Lax.ErrorNorm1(analitical, solution) << endl;
-	cout << "Err2: " << Lax.ErrorNorm2(analitical, solution) << endl;
-	cout << "ErrInf: " << Lax.ErrorNormInf(analitical, solution) << endl;
+	cout << "Err1: " << Lax.ErrorNorm1(analytical, solution) << endl;
+	cout << "Err2: " << Lax.ErrorNorm2(analytical, solution) << endl;
+	cout << "ErrInf: " << Lax.ErrorNormInf(analytical, solution) << endl;
 
-	Lax.printSolution(analitical);
+	Lax.printSolution(analytical);
 	
 	return 0;
 }
